e81b8c62b4df2e41e960d5d1e29da2301cb8a784
shaun hare <shaun@shaunhare.co.uk>
2018-12-25 08:24:12 +0000

    zsh

--- zsh/git-flow-completion.zsh
+++ zsh/git-flow-completion.zsh
@@ -344,3 +344,4 @@ __git_command_successful () {
 zstyle ':completion:*:*:git:*' user-commands flow:'provide high-level repository operations'
 alias gff='git branch -d `git branch --list "feature/*"`'
 alias gitdeletetag='git push origin :tagname'
+balias gtdel='git push origin :tagname'
