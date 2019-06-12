64d1aa1d5030078300465a00679014002e95b71e
Shaun Hare <shaun.hare@dvsa.gov.uk>
2019-06-12 07:32:42 +0100

    update to git config

--- zsh/git-flow-completion.zsh
+++ zsh/git-flow-completion.zsh
@@ -344,3 +344,4 @@ __git_command_successful () {
 zstyle ':completion:*:*:git:*' user-commands flow:'provide high-level repository operations'
 alias gff='git branch -d `git branch --list "feature/*"`'
 alias gitdeletetag='git push origin :tagname'
+alias grb='git for-each-ref --sort=-committerdate refs/heads/ --count=10'
