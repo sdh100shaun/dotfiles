3a7d8e4ee68a86c968e8b18363e8f63a3942f6f6
Shaun Hare <shaun.hare@dvsa.gov.uk>
2023-03-22 21:25:00 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
-# override zshrc
- rm ~/.zshrc && ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+
 
 #make sure brew installed at latest
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -10,6 +9,9 @@ brew install zplug
 brew tap homebrew/cask-fonts
 brew install font-inconsolata
 
+# override zshrc
+ rm ~/.zshrc && ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+ 
 #reload
 zsh 
 
