1ec771ae4d4fc60d72f315c657f4a5ceddcd9ff9
Shaun Hare <shaunhare@m-wy3j0v6n23.home>
2023-03-24 21:39:56 +0000

    fix zplug

--- setup.zsh
+++ setup.zsh
@@ -1,7 +1,16 @@
+#!/bin/zsh
 
-
+while getopts r:a:f: flag
+do
+    case "${flag}" in
+        r) brew=${OPTARG};;
+    esac
+done
+if [[ $brew = "brew"  ]]
+ then
 #make sure brew installed at latest
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 brew install zsh-completions
 brew install starship
@@ -10,13 +19,13 @@ brew tap homebrew/cask-fonts
 brew install font-inconsolata
 
 # override zshrc
- rm ~/.zshrc && ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
- 
-#reload
-zsh 
+ rm ~/.zshrc && ln -s ${PWD}/zsh/.zshrc ~/.zshrc
 
-if type zplug &> /dev/null; then 
+source $ZPLUG_HOME/init.zsh
+
+ if [[ (( $+commands[zplug] )) ]]
+ then 
 	echo "zplug installed adding plugins" 
-	zplug "rawkode/zsh-docker-run"
+	zplug  install "rawkode/zsh-docker-run"
 	zplug load "rawkode/zsh-docker-run"
 fi
