3d5d2de79ca70fc4dbb705d101dfe945cf37180b
Shaun Hare <shaun.hare@dvsa.gov.uk>
2026-03-29 18:06:31 +0100

    fix: git upstream setting

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,3 +1,5 @@
+# Kiro CLI pre block. Keep at the top of this file.
+[[ -f "${HOME}/Library/Application Support/kiro-cli/shell/zshrc.pre.zsh" ]] && builtin source "${HOME}/Library/Application Support/kiro-cli/shell/zshrc.pre.zsh"
 #set $PATH
 export PATH="/opt/homebrew/bin:$PATH"
 
@@ -15,9 +17,9 @@ source $ZPLUG_HOME/init.zsh
 #run starship
 eval "$(starship init zsh)"
 
-if [ -r ~/.zshrc ]; then echo 'export GPG_TTY=$(tty)' >> ~/.zshrc; \
-  else echo 'export GPG_TTY=$(tty)' >> ~/.zprofile; fi
-export GPG_TTY=$(tty)
+if [ -r ~/.zshrc ]; then echo ' ' >> ~/.zshrc; \
+  else echo ' ' >> ~/.zprofile; fi
+ 
 
 # run zoxide
 eval "$(zoxide init zsh)"
@@ -25,17 +27,40 @@ eval "$(zoxide init zsh)"
 
 source /Volumes/Development/tools/dotfiles/zsh/zsh-functions
 
+if [ -z "${USE_CONTAINERS}" ]; then
+	source /Volumes/Development/setup/dotfiles/zsh/zsh-functions
+	source /Volumes/Development/setup/dotfiles/zsh/containerised.zsh
+	zplug load "rawkode/zsh-docker-run"
+fi
 
-source /Volumes/Development/tools/dotfiles/zsh/zsh-functions
-source /Volumes/Development/tools/dotfiles/zsh/containerised.zsh
-zplug load "rawkode/zsh-docker-run"
+plugins=(
+ dotenv
+ git 
+ aws
+)
 
 source ~/.zshenv
 
-source /Volumes/Development/tools/dotfiles/zsh/aws_helper_profile.zsh
-
+source /Volumes/Development/setup/dotfiles/zsh/aws_helper_profile.zsh
+source /Volumes/Development/setup/dotfiles/zsh/aws-helper/ecs_helper.zsh
 eval "$(ssh-agent)"
-export GPG_TTY=$(tty)
+
+export PATH="/opt/homebrew/opt/gnu-getopt/bin:$PATH"
+
+export OLCS_APP_DIR="/Volumes/Development/projects/Vol/code/OLCS"
+
+
+# Created by `pipx` on 2024-09-18 07:37:05
+export PATH="$PATH:/Users/shaunhare/.local/bin"
+
+ export NVM_DIR="$HOME/.nvm"
+  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm b
+ 
+export PATH="/opt/homebrew/opt/mysql-client/bin:$PATH"
+ # Kiro CLI post block. Keep at the bottom of this file.
+[[ -f "${HOME}/Library/Application Support/kiro-cli/shell/zshrc.post.zsh" ]] && builtin source "${HOME}/Library/Application Support/kiro-cli/shell/zshrc.post.zsh"
+ 
 source /Volumes/Development/tools/dotfiles/zsh/zsh-functions
 source /Volumes/Development/tools/dotfiles/zsh/containerised.zsh
 zplug load "rawkode/zsh-docker-run"
--- zsh/containerised.zsh
+++ zsh/containerised.zsh
@@ -2,73 +2,81 @@
 
 # php container 
 
-function php() {
-  if [[ -v PHP_VERSION ]] 
-  then
-   run_with_docker "php" ${PHP_VERSION} "php" $@
-  else
-   run_with_docker "php" "latest" "php" $@
-  fi
-}
+if [ -z "${USE_CONTAINERS}" ]; then
+  function php() {
+    if [[ -v PHP_VERSION ]] 
+    then
+    run_with_docker "php" ${PHP_VERSION} "php" $@
+    else
+    run_with_docker "php" "latest" "php" $@
+    fi
+  }
 
-#terraform container 
-function terraform {
-   if [[ -v TF_VERSION ]]
-   then
-    run_with_docker "hashicorp/terraform" ${TF_VERSION} terraform $@
-   else
-     run_with_docker "hashicorp/terraform" "light" terraform $@
-   fi
-} 
+  #terraform container 
+  function terraform {
+    if [[ -v TF_VERSION ]]
+    then
+      run_with_docker "hashicorp/terraform" ${TF_VERSION} terraform $@
+    else
+      run_with_docker "hashicorp/terraform" "light" terraform $@
+    fi
+  } 
 
 
-#node and npm  containers 
-function node() {
-  if [[ -v NODE_VERSION ]]
-  then
-   run_with_docker "node" ${NODE_VERSION} "node" $@
-  else
-   run_with_docker "node" "latest" "node" $@
-  fi
-}
-
-function npm() {
+  #node and npm  containers 
+  function nodejs() {
     if [[ -v NODE_VERSION ]]
-  then
-    run_with_docker "node" ${NODE_VERSION}-"alpine" "npm" $@  
-  else
-    run_with_docker "node" "alpine" "npm" $@ 
-  fi
-}
-function npx() {
-    if [[ -v NODE_VERSION ]]
-  then
-    run_with_docker "node" ${NODE_VERSION}-"alpine" "npx" $@ 
-  else
-    run_with_docker "node" "alpine" "npx" $@
-  fi
-}
+    then
+    run_with_docker "node" ${NODE_VERSION} "node" $@
+    else
+    run_with_docker "node" "latest" "node" $@
+    fi
+  }
 
-# php composer
-function composer(){
-   run_with_docker "sdh100shaun/vol-composer" "latest" composer $@
-}
-
-#java container 
-
-
-function java() {
-  if [[ -v JAVA_VERSION ]] 
-  then
-   run_with_docker "amazoncorretto" ${JAVA_VERSION} "java" $@
-  else
-   run_with_docker "amazoncorretto" "latest" "java" $@
-  fi
-}
-
-function mvn() {
-  run_with_docker "maven" "latest" "mvn" $@
-  
-}
+  function npmjs() {
+      if [[ -v NODE_VERSION ]]
+    then
+      run_with_docker "node" ${NODE_VERSION}-"alpine" "npm" $@  
+    else
+      run_with_docker "node" "alpine" "npm" $@ 
+    fi
+  }
+  function npxjs() {
+      if [[ -v NODE_VERSION ]]
+    then
+      run_with_docker "node" ${NODE_VERSION}-"alpine" "npx" $@ 
+    else
+      run_with_docker "node" "alpine" "npx" $@
+    fi
+  }
 
 
+  # php composer
+  function vol-composer(){
+    run_with_docker "sdh100shaun/vol-composer" "latest" composer $@
+  }
+
+  function composer(){
+    run_with_docker "composer" "latest" composer $@
+  }
+  #java container 
+
+
+  #function java() {
+  #  if [[ -v JAVA_VERSION ]] 
+  #  then
+  #   run_with_docker "amazoncorretto" ${JAVA_VERSION} "java" $@
+  #  else
+  #   run_with_docker "amazoncorretto" "latest" "java" $@
+  # fi
+  #}
+
+  #function mvn() {
+  #  run_with_docker "maven" "latest" "mvn" $@
+  #  
+  #}
+
+  function pack() {
+  run_with_docker "buildpacksio/pack" "latest" "pack" $@
+  }
+fi
